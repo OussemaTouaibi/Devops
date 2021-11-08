87917585facde49839e107e1cc03ef37a92d11ef
FirasFoudhaili <firasfoudhailii@gmail.com>
2021-11-08 15:39:18 +0100

    log4j junit for department service

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring;
 
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
 
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
+
 		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRest1Application.class, args);
 	}
 
--- src/test/java/tn/esprit/spring/services/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/DepartementServiceImplTest.java
@@ -1,28 +1,43 @@
 package tn.esprit.spring.services;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.entities.Departement;
-
 import java.text.ParseException;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EntrepriseServiceImplTest {
+public class DepartementServiceImplTest {
+
     @Autowired
     IEntrepriseService es;
-   // private static final Logger l = LogManager.getLogger(EntrepriseServiceImplTest.class);
+
+    private static final Logger l = LogManager.getLogger(DepartementServiceImplTest.class);
+
+
 
     @Test
     public void ajouterDepartement() throws ParseException {
-        Departement d = new Departement("test");
+
+        Departement d = new Departement("dep");
         es.ajouterDepartement(d);
+        assertNotNull(d.getName());
+
+        l.info("department ");
+
+
+
     }
+
 }
